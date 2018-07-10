c685f211381541c366eb60d5813fdeeb702c37c8
Ubuntu <ubuntu@ip-172-31-24-173.ap-southeast-1.compute.internal>
2018-07-10 13:46:30 +0000

    오타 수정.

---
+++ services/kakao.service.js
@@ -0,0 +1,36 @@
+const Papago = require('../utils/Papago')
+const papago = new Papago()
+
+module.exports = class KakaoService {
+  changeLanguage(message) {
+
+    const splitMessages = message.split('->')
+    papago.sourceLangCode = splitMessages[0]
+    papago.targetLangCode = splitMessages[1]
+
+    return {
+      message: {
+        text: `${message}로 언어가 변경되었습니다.`
+      }
+    }
+  }
+
+  getTrLanguages() {
+    return {
+      message: {
+        text: '번역 가능한 언어목록'
+      },
+      keyboard: {
+        type: 'buttons',
+        buttons: ['한국어->영어', '한국어->중국어']
+      }
+    }
+  }
+
+  translateText(text) {
+    return papago.translateText(text)
+      .then(data => {
+        return { message: { text} }
+      })
+  }
+}
