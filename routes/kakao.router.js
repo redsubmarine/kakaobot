1bb893718462cf840ddf062d34d81b0089fede85
Ubuntu <ubuntu@ip-172-31-24-173.ap-southeast-1.compute.internal>
2018-07-10 13:07:41 +0000

    papago 연동

--- routes/kakao.router.js
+++ routes/kakao.router.js
@@ -1,10 +1,25 @@
 const router = require('express').Router()
+const Papago = require('../utils/Papago')
+
+const papago = new Papago()
 
 router.get('/keyboard', (req, res) => {
   res.send({
     type: 'buttons',
     buttons: ['번역언어 변경']
   })
+})
+
+router.post('/message', (req, res) => {
+  const { content, type } = req.body
+
+  papago.translate(content)
+    .then(data => {
+      const result = {
+        message: { text }
+      }
+      res.send(result)
+    })
 })
 
 module.exports = router
